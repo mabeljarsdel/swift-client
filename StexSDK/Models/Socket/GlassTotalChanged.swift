c313c923f5fc56480b1e498b5c3018dbb42fd30f
Alexander Rudyk <alakai.1998.28@gmail.com>
2019-09-05 17:49:49 +0300

    Create GlassTotalChanget model

---
+++ StexSDK/Models/Socket/GlassTotalChanged.swift
@@ -0,0 +1,30 @@
+//
+//  GlassTotalChanged.swift
+//  StexSDK
+//
+//  Created by Alexander Rudyk on 9/5/19.
+//  Copyright © 2019 beleven. All rights reserved.
+//
+
+import Foundation
+
+public class GlassTotalChanged: Codable {
+    /// Сurrency pair ID
+    public var pairId: Int
+    
+    /// Total glass value
+    public var total: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case pairId = "currency_pair_id"
+        case total
+    }
+    
+    required public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        pairId = try container.decode(Int.self, forKey: .pairId)
+        
+        total = try container.decodeStringToDouble(.total)
+    }
+}
