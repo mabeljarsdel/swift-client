2fe9483380e0c3e5041a618bf4d11c4771afdfc8
Hanna Karnaukh <hanna.karnaukh58@gmail.com>
2020-02-26 14:36:05 +0200

    Add NewsRequest

---
+++ StexSDK/Comunication/Request/Public/NewsRequest.swift
@@ -0,0 +1,20 @@
+//
+//  NewsRequest.swift
+//  StexSDK
+//
+//  Created by Hanna Karnaukh on 26.02.2020.
+//  Copyright © 2020 beleven. All rights reserved.
+//
+
+import Alamofire
+
+class NewsRequest: IRequest {
+
+    var httpMethod: HTTPMethod {
+        return .get
+    }
+    
+    var endpoint: String {
+        return PublicEndpoints.newsTwitter.path
+    }
+}
