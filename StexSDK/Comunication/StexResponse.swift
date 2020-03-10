2b004022d60dff62bacc46b4241501ac24a42141
Hanna Karnaukh <hanna.karnaukh58@gmail.com>
2020-03-10 19:38:05 +0200

    Separate StexResultError

--- StexSDK/Comunication/StexResponse.swift
+++ StexSDK/Comunication/StexResponse.swift
@@ -8,33 +8,6 @@
 
 import Foundation
 
-public struct StexResultError: Error {
-    
-    public private(set) var statusCode: Int
-    public private(set) var message: String
-    public private(set) var error: Error?
-    
-    public var localizedDescription: String {
-        return error?.localizedDescription ?? message
-    }
-    
-    static var unauthorized: StexResultError {
-        return StexResultError(statusCode: 401, message: "401 Unauthorized", error: nil)
-    }
-    
-    static func undefinedError(statusCode: Int, error: Error? = nil) -> StexResultError {
-        return StexResultError(statusCode: statusCode, message: "Oops! Something went wrong.", error: error)
-    }
-    
-    static func serverMessage(statusCode: Int, message: String?) -> StexResultError {
-        if let message = message {
-            return StexResultError(statusCode: statusCode, message: message, error: nil)
-        } else {
-            return undefinedError(statusCode: statusCode)
-        }
-    }
-}
-
 public struct StexResponse<T: Codable>: Codable {
     var isSuccess: Bool
     var message: String?
